68fd229ef1a50d34fab8f19499f121821f7f40e3
Ville Sebastian Olsson <seo@kth.se>
2025-06-11 14:13:34 +0200

    fix: Add missing serve.py

---
+++ tools/serve.py
@@ -0,0 +1,39 @@
+import logging
+from livereload import Server, shell
+
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="pybtex")
+
+
+def main():
+    server = Server()
+    files = [
+        "*.md",
+        "*.yml",
+        "images/*",
+    ]
+    for filename in files:
+        server.watch(filename, "make build")
+
+    livereload_logger = logging.getLogger("livereload")
+
+    max_attempts = 10
+    port = 8008
+    for i in range(max_attempts):
+        try:
+            server.serve(
+                root="_build/html",
+                port=port,
+                host="localhost",
+                open_url_delay=1,
+            )
+            break
+        except OSError:
+            if i == max_attempts - 1:
+                raise
+            port += 1
+            livereload_logger.handlers.clear()
+
+
+if __name__ == "__main__":
+    main()
